e899074769200b204cca7049a0ce5b0aa5941486
Reem Gawad <reemabdelg@gmail.com>
2022-08-26 11:36:39 -0400

    added simple input for tasks

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,16 @@
 import { v4 as uuidV4 } from "uuid"
 
-console.log(uuidV4())
+console.log('hi');
+
+type Task = {
+  id: string,
+  title: string,
+  completed: boolean,
+  createdAt: Date
+}
 
 const list = document.querySelector<HTMLUListElement>("#list")
-const form = document.getElementById("#new-task-form") as HTMLFormElement | null
+const form = document.getElementById("new-task-form") as HTMLFormElement | null
 const input = document.querySelector<HTMLInputElement>("#new-task-title")
 
 form?.addEventListener("submit", e => {
@@ -11,10 +18,22 @@ form?.addEventListener("submit", e => {
 
   if (input?.value == "" || input?.value == null) return
 
-  const task = {
+  const newTask: Task = {
     id: uuidV4(),
     title: input.value,
     completed: false,
     createdAt: new Date()
   }
+
+  addListItem(newTask)
 })
+
+function addListItem(task: Task) {
+  const item = document.createElement("li")
+  const label = document.createElement("label")
+  const checkbox = document.createElement("input")
+  checkbox.type = "checkbox"
+  label.append(checkbox, task.title)
+  item.append(label)
+  list?.append(item)
+}
